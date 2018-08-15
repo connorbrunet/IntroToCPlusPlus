5465bfde95d4646b8d97c7e8e98373e9964af722
Connor Brunet <s188040@students.aie.edu>
2018-08-15 18:03:17 -0500

    exersices

--- Variables/Source.cpp
+++ Variables/Source.cpp
@@ -4,7 +4,7 @@ int main()
 
 {
 
-	int number;
+	/*int number;
 	number = 7;
 	std::cout << number << std::endl;
 	std::cout << number + 17 << std::endl;
@@ -31,10 +31,75 @@ int main()
 	std::cin >> userInput;
 	std::cout << "You entered.. " << userInput << std::endl;
 
-	system("pause");
+	system("pause");*/
+
 	
 	//exercizes
 
+	// A)
+    // Expect 14, Actual 14
 
+	int numberA = 5;
 
+	numberA = 9;
+	numberA = 11;
+	numberA = 14;
+
+	std::cout << "A) " << numberA << std::endl;
+
+	// B)
+	// Expect 9, Actual 9
+
+	int numberB = 10;
+	numberB = 9;
+	int thingB = 55;
+	thingB = 22;
+	std::cout << "B) " << numberB << std::endl;
+
+	// C)
+	// Expect 21, Actual 21
+
+	int numberC = 3;
+	numberC = 7;
+	numberC = 1;
+	int somethingC = 23;
+	somethingC = 21;
+	numberC = somethingC;
+	std::cout << "C) " << numberC << std::endl;
+
+	// D)
+	// Expect 3, Actual 3
+
+	int numberD = 1;
+	int somethingD = 3;
+	numberD = somethingD;
+	std::cout << "D) " << somethingD << std::endl;
+	// E)
+	// Expect 6, Actual 6
+
+	int x = 13;
+	x = x / 2;
+	std::cout << "E) " << x << std::endl;
+
+	// F)
+	// Expect 6.5, Actual 6.5
+	float y = 13;
+	y = y / 2;
+	std::cout << "F)" << y << std::endl;
+
+	// Celsius to Fahrenheit)
+	float degCelsius = 78.0f;
+	float degFahrenheit = 0.0f; // Modify this variable below.
+	degCelsius x 1.8 + 32 =
+
+	std::cout << "Enter Celsius tempurature" << std::endl;
+	std::cin >> degCelsius;
+	std::cout << "Enter Fahrenhait tempurature" << std::endl;
+	std::cin >> degFahrenheit;
+
+	std::cout << "Celsius to Fahrenheit)" << std::endl;
+	std::cout << "Celsius: " << degCelsius << std::endl;
+	std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
+
+	system("pause");
 }
