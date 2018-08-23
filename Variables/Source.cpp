4164961bd6133e6a2df8127b170c7b3dfdf6c8fc
connorbrunet <connorbrunet@gmail.com>
2018-08-23 16:26:46 -0500

    asd

--- Variables/Source.cpp
+++ Variables/Source.cpp
@@ -4,173 +4,173 @@ int main()
 
 {
 	
-	/*
-
-	int number;
-	number = 7;
-	std::cout << number << std::endl;
-	std::cout << number + 17 << std::endl;
-	std::cout << number + 32 << std::endl;
-
-	int numA;
-	int numB;
-	int sum;
-	numA = 14;
-	numB = 2;
-	sum = numA + numB;
-	std::cout << sum << std::endl;
-
-	float numC;
-	float numD;
-	int sum2;
-	numC = 25.5f;
-	numD = 15.5f;
-	sum2 = numC + numD;
-	std::cout << sum2 << std::endl;
-
-	int userInput;
-	std::cout << "Input a whole number." << std::endl;
-	std::cin >> userInput;
-	std::cout << "You entered.. " << userInput << std::endl;
-
-	system("pause");
-
-	
 
 
+	//int number;
+	//number = 7;
+	//std::cout << number << std::endl;
+	//std::cout << number + 17 << std::endl;
+	//std::cout << number + 32 << std::endl;
 
-	// Exercizes
+	//int numA;
+	//int numB;
+	//int sum;
+	//numA = 14;
+	//numB = 2;
+	//sum = numA + numB;
+	//std::cout << sum << std::endl;
 
-	// A)
-    // Expect 14, Actual 14
+	//float numC;
+	//float numD;
+	//int sum2;
+	//numC = 25.5f;
+	//numD = 15.5f;
+	//sum2 = numC + numD;
+	//std::cout << sum2 << std::endl;
 
-	int numberA = 5;
+	//int userInput;
+	//std::cout << "Input a whole number." << std::endl;
+	//std::cin >> userInput;
+	//std::cout << "You entered.. " << userInput << std::endl;
 
-	numberA = 9;
-	numberA = 11;
-	numberA = 14;
+	//system("pause");
 
-	std::cout << "A) " << numberA << std::endl;
-
-	// B)
-	// Expect 9, Actual 9
-
-	int numberB = 10;
-	numberB = 9;
-	int thingB = 55;
-	thingB = 22;
-	std::cout << "B) " << numberB << std::endl;
-
-	// C)
-	// Expect 21, Actual 21
-
-	int numberC = 3;
-	numberC = 7;
-	numberC = 1;
-	int somethingC = 23;
-	somethingC = 21;
-	numberC = somethingC;
-	std::cout << "C) " << numberC << std::endl;
-
-	// D)
-	// Expect 3, Actual 3
-
-	int numberD = 1;
-	int somethingD = 3;
-	numberD = somethingD;
-	std::cout << "D) " << somethingD << std::endl;
-
-	// E)
-	// Expect 6, Actual 6
-
-	int x = 13;
-	x = x / 2;
-	std::cout << "E) " << x << std::endl;
-
-	// F)
-	// Expect 6.5, Actual 6.5
-	float y = 13;
-	y = y / 2;
-	std::cout << "F) " << y << std::endl;
+	//
 
 
 
-	// Celsius to Fahrenheit)
-	float degCelsius = 78.0f;
-	float degFahrenheit = 0.0f; // Modify this variable below.
+	//// Exercizes
 
-	std::cout << "Enter Celsius tempurature" << std::endl;
-	std::cin >> degCelsius;
-	degFahrenheit = degCelsius * 1.8 + 32;
+	//// A)
+ //   // Expect 14, Actual 14
 
-	std::cout << "Celsius to Fahrenheit)" << std::endl;
-	std::cout << "Celsius: " << degCelsius << std::endl;
-	std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
+	//int numberA = 5;
 
+	//numberA = 9;
+	//numberA = 11;
+	//numberA = 14;
 
+	//std::cout << "A) " << numberA << std::endl;
 
-	// Area of a Rectangle)
-	float rectWidth = 0.0f; // Modify this variable below.
-	float rectHeight = 0.0f; // Modify this variable below.
-	float rectArea = 0.0f; // Modify this variable below.
-						   // <Your work goes here>
+	//// B)
+	//// Expect 9, Actual 9
 
-	std::cout << "Enter Width" << std::endl;
-	std::cin >> rectWidth;
-	std::cout << "Enter Height" << std::endl;
-	std::cin >> rectHeight;
+	//int numberB = 10;
+	//numberB = 9;
+	//int thingB = 55;
+	//thingB = 22;
+	//std::cout << "B) " << numberB << std::endl;
 
-	rectArea = rectWidth * rectHeight;
+	//// C)
+	//// Expect 21, Actual 21
 
-	std::cout << "Area of a Rectangle)" << std::endl;
-	std::cout << "H: " << rectHeight << " , W: " << rectWidth << std::endl;
-	std::cout << "Area: " << rectArea << std::endl;
+	//int numberC = 3;
+	//numberC = 7;
+	//numberC = 1;
+	//int somethingC = 23;
+	//somethingC = 21;
+	//numberC = somethingC;
+	//std::cout << "C) " << numberC << std::endl;
 
-	// Average of Five)
-	float a, b, c, d, e;	// Modify these variables below.
-	float avg;				// Modify this variable below.
-	avg = a = b = c = d = e = 0.0f; // Initialize all to zero.
-											// <Your work goes here>
+	//// D)
+	//// Expect 3, Actual 3
 
-	std::cout << "Enter 5 Numbers" << std::endl;
-	std::cin >> a;
-	std::cin >> b;
-	std::cin >> c;
-	std::cin >> d;
-	std::cin >> e;
-	avg = (a + b + c + d + e) / 5;
+	//int numberD = 1;
+	//int somethingD = 3;
+	//numberD = somethingD;
+	//std::cout << "D) " << somethingD << std::endl;
 
-	std::cout << "Average of Five)" << std::endl;
-	std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
-	std::cout << "avg: " << avg << std::endl;
+	//// E)
+	//// Expect 6, Actual 6
 
+	//int x = 13;
+	//x = x / 2;
+	//std::cout << "E) " << x << std::endl;
 
-
-	// Number Swap)
-
-	int x = 13;
-	int y = 24;
-
-	// <Your work goes here>
-
-	int temp;
-
-	std::cout << "Enter A Number for X" << std::endl;
-	std::cin >> x;
-	std::cout << "Enter A Number for Y" << std::endl;
-	std::cin >> y;
-
-	temp = x;
-	x = y;
-	y = temp;
-
-	std::cout << "Number Swap)" << std::endl;
-	std::cout << "x is " << x << std::endl;
-	std::cout << "y is " << y << std::endl;
+	//// F)
+	//// Expect 6.5, Actual 6.5
+	//float y = 13;
+	//y = y / 2;
+	//std::cout << "F) " << y << std::endl;
 
 
 
-	// Fun Facts for Your Age)
+	//// Celsius to Fahrenheit)
+	//float degCelsius = 78.0f;
+	//float degFahrenheit = 0.0f; // Modify this variable below.
+
+	//std::cout << "Enter Celsius tempurature" << std::endl;
+	//std::cin >> degCelsius;
+	//degFahrenheit = degCelsius * 1.8 + 32;
+
+	//std::cout << "Celsius to Fahrenheit)" << std::endl;
+	//std::cout << "Celsius: " << degCelsius << std::endl;
+	//std::cout << "Fahrenheit: " << degFahrenheit << std::endl;
+
+
+
+	//// Area of a Rectangle)
+	//float rectWidth = 0.0f; // Modify this variable below.
+	//float rectHeight = 0.0f; // Modify this variable below.
+	//float rectArea = 0.0f; // Modify this variable below.
+	//					   // <Your work goes here>
+
+	//std::cout << "Enter Width" << std::endl;
+	//std::cin >> rectWidth;
+	//std::cout << "Enter Height" << std::endl;
+	//std::cin >> rectHeight;
+
+	//rectArea = rectWidth * rectHeight;
+
+	//std::cout << "Area of a Rectangle)" << std::endl;
+	//std::cout << "H: " << rectHeight << " , W: " << rectWidth << std::endl;
+	//std::cout << "Area: " << rectArea << std::endl;
+
+	//// Average of Five)
+	//float a, b, c, d, e;	// Modify these variables below.
+	//float avg;				// Modify this variable below.
+	//avg = a = b = c = d = e = 0.0f; // Initialize all to zero.
+	//										// <Your work goes here>
+
+	//std::cout << "Enter 5 Numbers" << std::endl;
+	//std::cin >> a;
+	//std::cin >> b;
+	//std::cin >> c;
+	//std::cin >> d;
+	//std::cin >> e;
+	//avg = (a + b + c + d + e) / 5;
+
+	//std::cout << "Average of Five)" << std::endl;
+	//std::cout << a << "," << b << "," << c << "," << d << "," << e << std::endl;
+	//std::cout << "avg: " << avg << std::endl;
+
+
+
+	//// Number Swap)
+
+	//int x = 13;
+	//int y = 24;
+
+	//// <Your work goes here>
+
+	//int temp;
+
+	//std::cout << "Enter A Number for X" << std::endl;
+	//std::cin >> x;
+	//std::cout << "Enter A Number for Y" << std::endl;
+	//std::cin >> y;
+
+	//temp = x;
+	//x = y;
+	//y = temp;
+
+	//std::cout << "Number Swap)" << std::endl;
+	//std::cout << "x is " << x << std::endl;
+	//std::cout << "y is " << y << std::endl;
+
+
+
+	//// Fun Facts for Your Age)
 
 	int age = 0;
 
@@ -199,58 +199,59 @@ int main()
 
 
 
-	// Conforming to Conventions)
-	int age;
-	int bullet_count;
-	float dog_years;
-	int qty;
-	float shield_value;
-	float defense_matrix_cooldown;
-	int red_armor_value;
-	float red_armor_ratio;
-	int happiness;
-	int gandhi_aggression;
+	//// Conforming to Conventions)
 
-	// Challenges
-
-
-	// The Right Tool for the Job)
-
-	//int // 16-bit integer
-	//long // 32-bit integer
-	//unsigned // can only store positive numbers
-	//signed // can only store positive numbers
-	//char // stores one character, takes 1 byte of memory
-	//float // single 32-bit precision floating point, accomodates up to 7 digits
-	//double // 64-bit precision floating point, accomodates up to 15-16 digits
-	//long double // larger than double
-	//bool // stores true or false value
-	//void // no storage
+	//int age;
+	//int bullet_count;
+	//float dog_years;
+	//int qty;
+	//float shield_value;
+	//float defense_matrix_cooldown;
+	//int red_armor_value;
+	//float red_armor_ratio;
+	//int happiness;
+	//int gandhi_aggression;
 
 
-	// Number Swap (Difficulty Up)
 
-	int x = 13;
-	int y = 24;
+	//// Challenges
 
-	// <Your work goes here>
 
-	std::cout << "Enter A Number for X" << std::endl;
-	std::cin >> x;
-	std::cout << "Enter A Number for Y" << std::endl;
-	std::cin >> y;
+	//// The Right Tool for the Job)
 
-	x = x + y;
-	y = x - y;
-	x = x - y;
+	////int // 16-bit integer
+	////long // 32-bit integer
+	////unsigned // can only store positive numbers
+	////signed // can only store positive numbers
+	////char // stores one character, takes 1 byte of memory
+	////float // single 32-bit precision floating point, accomodates up to 7 digits
+	////double // 64-bit precision floating point, accomodates up to 15-16 digits
+	////long double // larger than double
+	////bool // stores true or false value
+	////void // no storage
 
-	std::cout << "Number Swap)" << std::endl;
-	std::cout << "x is " << x << std::endl;
-	std::cout << "y is " << y << std::endl;
+
+	//// Number Swap (Difficulty Up)
+
+	//int x = 13;
+	//int y = 24;
+
+	//// <Your work goes here>
+
+	//std::cout << "Enter A Number for X" << std::endl;
+	//std::cin >> x;
+	//std::cout << "Enter A Number for Y" << std::endl;
+	//std::cin >> y;
+
+	//x = x + y;
+	//y = x - y;
+	//x = x - y;
+
+	//std::cout << "Number Swap)" << std::endl;
+	//std::cout << "x is " << x << std::endl;
+	//std::cout << "y is " << y << std::endl;
 
 
 	system("pause");
-
-	*/
 
 }
