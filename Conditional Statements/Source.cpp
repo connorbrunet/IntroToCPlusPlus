6f2f5210be4097fc731f879a18933522c23d90ee
connorbrunet <connorbrunet@gmail.com>
2018-08-26 14:39:53 -0500

    homework finished

--- Conditional Statements/Source.cpp
+++ Conditional Statements/Source.cpp
@@ -117,22 +117,19 @@ int main()
 	}
 
 	//Homework
+
 	{
+
+		//1)
+
+		/*int y, x;
+		y = 0;
+		if (y == 0)
 		{
-
-			//1)
-
-			/*int y, x;
-			y = 0;
-
-			if (y == 0)
-			{
-				x = 100;
-			}
-
-			std::cout << x << std::endl;*/
-
+			x = 100;
 		}
+		std::cout << x << std::endl;*/
+
 
 		//2)
 
@@ -148,109 +145,126 @@ int main()
 		else
 			std::cout << num1;*/
 
-			//3) 
+		//3) 
 
-			int a, b, c, d, e;
-			//Poll input for five values
-			std::cout << "Enter 5 Numbers";
-			std::cin >> a >> b >> c >> d >> e;
+		//i don't understand how to do this
+
+		/*int a, b, c, d, e;
+		int num1, num2, num3, num4, num5;
+		int sum;
+		//Poll input for five values
+		std::cout << "Enter 5 Numbers";
+		std::cin >> a >> b >> c >> d >> e;
+		sum = a + b + c + d + e;
 			//Check if positive sum
 				//Sort in ascending order
-				//Find greatest value - a
-					//Find 2nd value - b
-						//Find 3rd value - c
-							//Find 4th value - d
-								//Find 5th value				
-							//Find 4th value - e
-								//Find 5th value
-						//Find 3rd value - d
-							//Find 4th value
-								//Find 5th value
-						//Find 3rd value - e
-							//Find 4th value
-								//Find 5th value				
-					//Find 2nd value - c
-						//Find 3rd value
-							//Find 4th value
-								//Find 5th value
-					//Find 2nd value - d
-						//Find 3rd value
-							//Find 4th value
-								//Find 5th value
-					//Find 2nd value - e
-						//Find 3rd value
-							//Find 4th value
-								//Find 5th value
 			//else if negative sum
 				//sort in ascending order
 			//else
 				//output in same order input
 
-
-			if (a > b && b > c && c > d && d > e)
+		if (sum > 0)
+		{
+			if (a < b && a < c && a < d && a < e)
 			{
-				std::cout << a << b << c << d << e << std::endl;
+				num1 = a;
+				if (b < c && b < d && b < e)
+				{
+					num2 = b;
+					if (c < d && c < e)
+					{
+						num3 = c;
+						if (d < e)
+						{
+							num4 = d;
+							num5 = e;
+						}
+						else
+						{
+							num4 = e;
+							num5 = d;
+						}
+					}
+					else if (d < c && d < e)
+					{
+						num3 = d;
+						if (c < e)
+						{
+							num4 = c;
+							num5 = e;
+						}
+						else
+						{
+							num4 = e;
+							num5 = c;
+						}
+					}
+				}
 			}
+		}
+		std::cout << num1 << num2 << num3 << num4 << num5 << std::endl;*/
 
-			//4)
 
-			/*int userInput;
-			std::cout << "Enter 1 2 3 or 4\n";
-			std::cin >> userInput;
-			switch (userInput)
-			{
-			case 1:
-				std::cout << "1\n" << std::endl;
-				break;
-			case 2:
-				std::cout << "2 or 3\n" << std::endl;
-				break;
-			case 3:
-				std::cout << "2 or 3\n" << std::endl;
-				break;
-			case 4:
-				std::cout << "4\n" << std::endl;
-				break;
-			default:
-				std::cout << "Invalid\n" << std::endl;
-				break;*/
 
-				//5)
+		//4)
 
-				//result = (value1 < value2) ? 24 : 16;
+		/*int userInput;
+		std::cout << "Enter 1 2 3 or 4\n";
+		std::cin >> userInput;
+		switch (userInput)
+		{
+		case 1:
+			std::cout << "1\n" << std::endl;
+			break;
+		case 2:
+			std::cout << "2 or 3\n" << std::endl;
+			break;
+		case 3:
+			std::cout << "2 or 3\n" << std::endl;
+			break;
+		case 4:
+			std::cout << "4\n" << std::endl;
+			break;
+		default:
+			std::cout << "Invalid\n" << std::endl;
+			break;*/
 
-				//6)
+		//5)
 
-			/*int num1, num2;
-			char operation;
+		//result = (value1 < value2) ? 24 : 16;
 
-			std::cout << "enter 2 numbers\n";
-			std::cin >> num1 >> num2;
-			std::cout << "add(+), subtract(-), multiply(*), divide(/) or mod(%)\n";
-			std::cin >> operation;
+		//6)
 
-			switch (operation)
-			{
-			case '+':
-				std::cout << num1 + num2 << std::endl;
-				break;
-			case '-':
-				std::cout << num1 - num2 << std::endl;
-				break;
-			case '*':
-				std::cout << num1 * num2 << std::endl;
-				break;
-			case '/':
-				std::cout << num1 / num2 << std::endl;
-				break;
-			case '%':
-				std::cout << num1 % num2 << std::endl;
-				break;
-			}*/
+		/*int num1, num2;
+		char operation;
+
+		std::cout << "enter 2 numbers\n";
+		std::cin >> num1 >> num2;
+		std::cout << "add(+), subtract(-), multiply(*), divide(/) or mod(%)\n";
+		std::cin >> operation;
+
+		switch (operation)
+		{
+		case '+':
+			std::cout << num1 + num2 << std::endl;
+			break;
+		case '-':
+			std::cout << num1 - num2 << std::endl;
+			break;
+		case '*':
+			std::cout << num1 * num2 << std::endl;
+			break;
+		case '/':
+			std::cout << num1 / num2 << std::endl;
+			break;
+		case '%':
+			std::cout << num1 % num2 << std::endl;
+			break;
+		}*/
 
 		//7)
 
-		int month;
+		/*int month;
 		std::cout << "input a number that represents a month of the year" << std::endl;
 		std::cin >> month;
 		switch (month)
@@ -290,9 +304,34 @@ int main()
 			break;
 		case 12:
 			std::cout << "31 days." << std::endl;
-			break;
+			break;*/
 
-		}
+
+		//8)
+
+		/*bool flag = true;
+		int numPos = 35, numNeg = -55;
+		char frstChar = 'J', scndChar = 'O';
+		double frstPrice = 5.60;
+
+		/*a. numPos > numNeg - true
+		b. frstChar > scndChar - false
+		c. !(numPos + numNeg) - false
+		d. (numPos == -30) || (numNeg == -55) - true
+		e. (frstPrice >= 4.1) && (frstPrice <= 9.9) - true
+		f. !flag && (scndChar <= ‘R’) - false
+		g. (numPos < 66) || (flag && numPos > 35) - true
+		h. ++numPos == 36 - true
+		i. numPos++ == 36 - true
+		j. (frstChar == ‘j’) || (frstChar == ‘J’) - true*/
+
+		//9)
+
+		/*a. (a || b) || (a && b) - true
+		b. !((!a) && (a)) || (a && b) - true
+		c. !((5 || a) || (!b)) && (!(a) && b) - false
+		d. a || b && a - true
+		e. !a&&b - true*/
 
 	}
 
