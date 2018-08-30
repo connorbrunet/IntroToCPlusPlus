894cd364aaa58e2e4219db7791f9549ed4a370a9
connorbrunet <connorbrunet@gmail.com>
2018-08-30 02:21:14 -0500

    dont commit

--- Loops/Source.cpp
+++ Loops/Source.cpp
@@ -2,15 +2,15 @@
 
 int main()
 {
-	{
+	/*{
 		int count;
 		for (int count = 0; count < 10; count++)
 		{
 			std::cout << count << std::endl;
 		}
-	}
+	}*/
 
-	{
+	/*{
 		int i = 1;
 		while (i <= 10)
 		{
@@ -22,7 +22,7 @@ int main()
 			}
 		}
 		system("cls");
-	}
+	}*/
 
 	/*{
 		char input;
@@ -40,16 +40,38 @@ int main()
 		system("cls");
 	}*/
 
-	{
+	/*{
 		int i = 0;
 		for (int i = 0; i < 12; i += 2)
 			if (i <= 12)
 			{
 				std::cout << i << std::endl;
 			}
+	}*/
+
+	//Homework
+
+	int input = 0;
+	std::cout << "Enter a number\n";
+	std::cin >> input;
+
+	if (input % 5 == 0 && input % 3 == 0)
+	{
+		std::cout << "FizzBuzz\n";
+	}
+	else if (input % 3 == 0)
+	{
+		std::cout << "Fizz\n";
 	}
 
-
+	else if (input % 5 == 0)
+	{
+		std::cout << "Buzz\n";
+	}
+	else
+	{
+		std::cout << input << std::endl;
+	}
 
 	system("pause");
 }
